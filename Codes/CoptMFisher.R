592c67a76be70d34a0d68f55bd962ac7acefc135
DOE_CoCa <theseadama@gmail.com>
2025-06-05 14:19:08 +0200

    Update

--- Codes/CoptMFisher.R
+++ Codes/CoptMFisher.R
@@ -1,17 +1,17 @@
 CoptMFisher <- function(X, model, sigeps, Dx, Dtheta, dftheta, typeCopt="Det", L=1000, type='SK'){
   
   if(!is.matrix(X)) stop("X doit etre une matrice ! ")
-  THETA = Unif(K, Dtheta)
+  THETA = Unif(L, Dtheta)
   C <- rep(0, L)
   for(l in 1:L){
     R = predict(model, newdata=Dbind(X,THETA[l,]), type=type, 
                 cov.compute = TRUE, se.compute = FALSE, checkNames = FALSE)
-    Covmat <- R$cov + diag(sigeps, n)
+    Covmat <- R$cov + diag(sigeps, nrow(X))
     Mat <- Fishermatrix(X, THETA[l,], dftheta, Covmat=Covmat)
     if (typeCopt=="Det")      C[l] <- det(Mat)
-    if (typeCopt=="Tr")       C[l] <- tr(Mat)
+    if (typeCopt=="Tr")       C[l] <- sum(diag(Mat))
     if (typeCopt=="DetInv")   C[l] <- det(solve(Mat))
-    if (typeCopt=="TrInv")    C[l] <- tr(solve(Mat))
+    if (typeCopt=="TrInv")    C[l] <- sum(diag(solve(Mat)))
   }
   
   Cmean <- mean(C)
